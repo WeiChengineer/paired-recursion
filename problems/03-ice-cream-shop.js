fcf067bdcb8dcdc6eeed0aeacd2e01e7b5c7af9e
EndlerGuppy <JenniferLaCoco@gmail.com>
2023-08-22 22:19:55 -0600

    p3

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -5,18 +5,23 @@ user's favorite ice cream flavor. Recursively find out whether or not the shop
 offers their favorite flavor.
 
 Examples:
-iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
-iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
-iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
-iceCreamShop(['moose tracks'], 'moose tracks'); // true
-iceCreamShop([], 'honey lavender'); // false
 ***********************************************************************/
 
 
 function iceCreamShop(flavors, favorite) {
-  // Your code here 
+  if (flavors.length === 0) {
+    return false;
+  }
+  if (flavors[0] === favorite) return true;
+  return iceCreamShop(flavors.slice(1), favorite)
+
 }
 
+console.log(iceCreamShop(['vanilla', 'strawberry'], 'blue moon')); // false
+console.log(iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')); // true
+console.log(iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')); // false
+console.log(iceCreamShop(['moose tracks'], 'moose tracks')); // true
+console.log(iceCreamShop([], 'honey lavender')); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
